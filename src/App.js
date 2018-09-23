99da24d6527fc3289812d8012f9572b45865ff71
Brianne <brianne.hennel@gmail.com>
2018-09-23 16:21:56 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import axios from 'axios'
+
+class App extends Component {
+
+  state = {
+    venues: []
+  }
+
+  componentDidMount() {
+    this.getVenues()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDIZ-5GLvu0xUA2f6vwt82p2R8W2au8aDc&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "L5DKG5X0DKH5JYOCWVFLRP1UPSWZJKJY51HYMEK2CFCSD5KV",
+      client_secret: "APCJA50MQOTWPKJXYABRI0C1NQZCHL0VD40NWAEODBRSEKQU",
+      query: "food",
+      near: "Sydney",
+      v: "20182507"
+    }
+
+//like fetchAPI
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        }, this.renderMap())
+      })
+      .catch(error => {
+        console.log("ERROR! " + error)
+      })
+  }
+
+  initMap = () => {
+
+    // Create A Map
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    })
+
+    // Create An InfoWindow
+    var infowindow = new window.google.maps.InfoWindow()
+    // Display Dynamic Markers
+    this.state.venues.map(myVenue => {
+
+      var contentString = `${myVenue.venue.name}`
+
+      // Create A Marker
+      var marker = new window.google.maps.Marker({
+        position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
+        map: map,
+        title: myVenue.venue.name
+      })
+
+      // Click on a marker
+      marker.addListener('click', function() {
+        // Change the content
+        infowindow.setContent(contentString)
+        // Open An InfoWindow
+        infowindow.open(map, marker);
+      })
+    })
+  }
+
+  render() {
+    return (
+      <main>
+        <div id="map"></div>
+      </main>
+    );
+  }
+}
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
+export default App;
